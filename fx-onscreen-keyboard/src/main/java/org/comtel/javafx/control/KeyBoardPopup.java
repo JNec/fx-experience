e3c28a6db6cea9c56ef0755cd3b1e77d18431746
JNec <jankovski@gmail.com>
2015-03-02 13:38:54 +0100

    Keep track of focused node
    Keep track of focused node

--- fx-onscreen-keyboard/src/main/java/org/comtel/javafx/control/KeyBoardPopup.java
+++ fx-onscreen-keyboard/src/main/java/org/comtel/javafx/control/KeyBoardPopup.java
@@ -1,45 +1,54 @@
 package org.comtel.javafx.control;
 
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Popup;
 
 public class KeyBoardPopup extends Popup {
 
-	private final KeyboardPane keyboard;
+  private final KeyboardPane keyboard;
+  private Scene owner = null;
+  private Node nodeOwner = null;
 
-	private Scene owner = null;
+  public final KeyboardPane getKeyBoard() {
+    return keyboard;
+  }
 
-	public final KeyboardPane getKeyBoard() {
-		return keyboard;
-	}
+  public KeyBoardPopup(KeyboardPane panel) {
+    keyboard = panel;
+    getContent().add(panel);
+  }
 
-	public KeyBoardPopup(KeyboardPane panel) {
-		keyboard = panel;
-		getContent().add(panel);
-	}
+  public boolean isVisible() {
+    return isShowing();
+  }
 
-	public boolean isVisible() {
-		return isShowing();
-	}
+  public void setVisible(boolean b) {
+    if (b) {
+      if (owner != null) {
+        super.show(owner.getWindow());
+      } else {
+        super.show(this.getOwnerWindow());
+      }
+    } else {
+      hide();
+    }
 
-	public void setVisible(boolean b) {
-		if (b) {
-			if (owner != null) {
-				super.show(owner.getWindow());
-			} else {
-				super.show(this.getOwnerWindow());
-			}
-		} else {
-			hide();
-		}
+  }
 
-	}
+  public Scene getOwner() {
+    return owner;
+  }
 
-	public Scene getOwner() {
-		return owner;
-	}
+  public void setOwner(Scene owner) {
+    this.owner = owner;
+  }
 
-	public void setOwner(Scene owner) {
-		this.owner = owner;
-	}
+  public Node getNodeOwner() {
+    return nodeOwner;
+  }
+
+  public void setNodeOwner(Node nodeOwner) {
+    this.nodeOwner = nodeOwner;
+  }
 }
